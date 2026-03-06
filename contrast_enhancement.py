6db123ddd3bf61c257cd57e7f0cd92a7d1c603b4
imageprocessing2403 <imageprocessing2403@gmail.com>
2026-03-06 14:50:15 +0530

    Update contrast_enhancement.py

--- contrast_enhancement.py
+++ contrast_enhancement.py
@@ -1,9 +1,13 @@
 import cv2
+import os
 
-def enhance_contrast(image):
+in_dir = "03_denoised"
+out_dir = "04_contrast"
+os.makedirs(out_dir, exist_ok=True)
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+for img_name in os.listdir(in_dir):
+    img = cv2.imread(f"{in_dir}/{img_name}", 0)
+    enhanced = cv2.equalizeHist(img)
+    cv2.imwrite(f"{out_dir}/{img_name}", enhanced)
 
-    equalized = cv2.equalizeHist(gray)
-
-    return equalized
+print("Contrast enhancement completed")
